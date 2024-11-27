3d4d79d77eb22f37dd52c31b6924928a77d01a37
manishdevaraj <manishdevaraj03@gmail.com>
2024-11-27 20:29:31 +0530

    manish commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Demo from "./Components/Demo"
+import Hero from "./Components/Hero"
+
+
+function App() {
+  return (
+    <main>
+      <div className="main">
+        <div className="gradient"/>
+
+      
+      </div>
+      <div className="app">
+        <Hero/>
+        <Demo/>
+      </div>
+
+
+    </main>
+  )
+}
+
+export default App
